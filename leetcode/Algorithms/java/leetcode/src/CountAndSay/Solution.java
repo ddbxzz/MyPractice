95f6a550626a853a42e874fafc50cbdc6314e23b
dengdaibuxizhizhou <sima720@163.com>
2021-01-28 21:35:48 +0800

    Count and Say

--- leetcode/Algorithms/java/leetcode/src/CountAndSay/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/CountAndSay/Solution.java
@@ -1,14 +1,64 @@
 package CountAndSay;
 
+import org.junit.Test;
+
 public class Solution {
+    //循环
+    public String countAndSay1(int n) {
+        StringBuilder result = new StringBuilder().append("1");
+
+        if (n == 1) {
+            return result.toString();
+        }
+
+        for (int i = 1; i < n; i++) {
+            StringBuilder tmp = new StringBuilder();
+
+            int start = 0;
+            int j = 0;
+            for (; j < result.length(); j++) {
+                if (result.charAt(start) != result.charAt(j)) {
+                    tmp.append(j - start).append(result.charAt(start));
+                    start = j;
+                }
+            }
+            tmp.append(j - start).append(result.charAt(start));
+
+            result = tmp;
+        }
+
+        return result.toString();
+    }
+
+    //递归
     public String countAndSay(int n) {
         if (n == 1) {
             return "1";
         }
-        if (n == 2) {
-            return "11";
-        }
 
-        return null;
+        String str = countAndSay(n - 1);
+        StringBuilder result = new StringBuilder();
+        int start = 0;
+        int i = 0;
+        for (; i < str.length(); i++) {
+            if (str.charAt(start) != str.charAt(i)) {
+                result.append(i - start).append(str.charAt(start));
+                start = i;
+            }
+        }
+        result.append(i - start).append(str.charAt(start));
+
+        return result.toString();
+    }
+
+
+    @Test
+    public void test() {
+        assert ("1".equals(countAndSay1(1)));
+        assert ("11".equals(countAndSay1(2)));
+        assert ("21".equals(countAndSay1(3)));
+        assert ("312211".equals(countAndSay1(6)));
+        assert ("13112221".equals(countAndSay1(7)));
+
     }
 }
