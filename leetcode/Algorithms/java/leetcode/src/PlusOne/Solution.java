93252a1d8d11c8f31c40d84d3b6e4c1926393740
dengdaibuxizhizhou <sima720@163.com>
2021-01-31 14:55:54 +0800

    Plus One

--- leetcode/Algorithms/java/leetcode/src/PlusOne/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/PlusOne/Solution.java
@@ -32,10 +32,23 @@ public class Solution {
         return digits;
     }
 
+    public int[] plusOne1(int[] digits) {
+        int len = digits.length;
+        for(int i = len - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] %= 10;
+            if(digits[i] != 0)
+                return digits;
+        }
+        digits = new int[len + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
     @Test
     public void test() {
         int[] digits = {9, 9, 9};
-        int[] res = plusOne(digits);
+        int[] res = plusOne1(digits);
         for (int i : res) {
             System.out.println(i);
         }
