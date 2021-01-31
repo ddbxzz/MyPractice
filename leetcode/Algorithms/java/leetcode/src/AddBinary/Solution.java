f6bf74c19d7a1781b20cdfa63e04aee2ce777e55
dengdaibuxizhizhou <sima720@163.com>
2021-01-31 17:59:14 +0800

    Add Binary

--- leetcode/Algorithms/java/leetcode/src/AddBinary/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/AddBinary/Solution.java
@@ -4,47 +4,20 @@ import org.junit.Test;
 
 public class Solution {
     public String addBinary(String a, String b) {
-        int len;
-        StringBuilder str = new StringBuilder();
-        String tmpStr;
         int lenA = a.length();
         int lenB = b.length();
-        if (a.length() <= b.length()) {
-            len = a.length();
-            tmpStr = b;
-        }
-        else {
-            len = b.length();
-            tmpStr = a;
-        }
-
-        int sum;
+        int maxLen = (lenA < lenB) ? lenB : lenA;
+        StringBuilder str = new StringBuilder();
         int carry = 0;
-        for (int i = lenA - 1, j = lenB - 1; i >= 0 && j >= 0; i--, j--) {
-            sum = (a.charAt(i) - '0') + (b.charAt(j) - '0') + carry;
-            if (sum > 1) {
-                sum /= 3;
-                carry = 1;
-            }
-            else {
-                carry = 0;
-            }
-            str.append((sum));
+
+        for (int i = 0; i < maxLen; i++) {
+            carry += (i < lenA) ? (a.charAt(lenA - i - 1) - '0') : 0;
+            carry += (i < lenB) ? (b.charAt(lenB - i - 1) - '0') : 0;
+            str.append(carry % 2);
+            carry /= 2;
         }
 
-        int appendLen = tmpStr.length() - len;
-        for (int i = appendLen - 1; i >= 0; i--) {
-            sum = (tmpStr.charAt(i) - '0') + carry;
-            if (sum > 1) {
-                sum = 0;
-                carry = 1;
-            }
-            else {
-                carry = 0;
-            }
-            str.append((sum));
-        }
-        if (carry > 0) {
+        if (carry == 1) {
             str.append(carry);
         }
 
@@ -53,8 +26,8 @@ public class Solution {
 
     @Test
     public void test() {
-        String a = "100";
-        String b = "110010";
+        String a = "111";
+        String b = "111111";
 
 
         System.out.println(addBinary(a, b));
