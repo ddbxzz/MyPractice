a7c500ee6be86816d6bfb7f0b6ab342432041c25
dengdaibuxizhizhou <sima720@163.com>
2021-02-05 17:07:29 +0800

    Pascal's Triangle II

--- leetcode/Algorithms/java/leetcode/src/PascalTriangleII/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/PascalTriangleII/Solution.java
@@ -9,13 +9,9 @@ public class Solution {
         List<Integer> list = new ArrayList<>(rowIndex + 1);
         list.add(0, 1);
         for (int i = 1; i <= rowIndex; i++) {
-            for (int j = i; j >= 0; j--) {
+            for (int j = i; j > 0; j--) {
                 if (j == i) {
                     list.add(j, 1);
-                    continue;
-                }
-                if (j == 0) {
-                    list.set(j, 1);
                     continue;
                 }
                 list.set(j, list.get(j) + list.get(j -1));
