e807e945a9d65e20384d75bde56632fea5e8faa5
dengdaibuxizhizhou <sima720@163.com>
2021-03-01 08:20:39 +0800

    Generate Parentheses(back track)

--- leetcode/Algorithms/java/leetcode/src/GenerateParentheses/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/GenerateParentheses/Solution.java
@@ -8,15 +8,16 @@ import java.util.List;
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generateAll(new char[2 * n], 0, result);
+        //generateAll(new char[2 * n], 0, result);
+        backtrack(result, new StringBuilder(), 0, 0, n);
         return result;
     }
 
+    //Brute Force
     private void generateAll(char[] p, int pos, List<String> list) {
         if (pos == p.length) {
             if (valid(p)) {
                 list.add(new String(p));
-
             }
             return;
         }
@@ -44,9 +45,29 @@ public class Solution {
         return banc == 0;
     }
 
+    //back track
+    public void backtrack(List<String> ans, StringBuilder sb, int open, int close, int max) {
+        if (sb.length() == 2 * max) {
+            ans.add(sb.toString());
+            return;
+        }
+
+        if (open < max) {
+            sb.append('(');
+            backtrack(ans, sb, open + 1, close, max);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if (close < open) {
+            sb.append(')');
+            backtrack(ans, sb, open, close + 1, max);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
     @Test
     public void test() {
-        List<String> strings = generateParenthesis(3);
+        List<String> strings = generateParenthesis(2);
         for (String s : strings) {
             System.out.println(s);
         }
