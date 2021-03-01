d0046ad3f3a650d3960fef32622c1bc5ce918e0e
dengdaibuxizhizhou <sima720@163.com>
2021-03-01 13:58:24 +0800

    Swap Nodes in Pairs

--- leetcode/Algorithms/java/leetcode/src/SwapNodesPairs/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/SwapNodesPairs/Solution.java
@@ -1,5 +1,7 @@
 package SwapNodesPairs;
 
+import java.util.List;
+
 class ListNode {
     int val;
     ListNode next;
@@ -11,5 +13,37 @@ class ListNode {
 public class Solution {
     public ListNode swapPairs(ListNode head) {
 
+        return swap1(head);
     }
+
+    //递归
+    private ListNode swap1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode next = head.next;
+        head.next = swap1(next.next);
+
+        next.next = head;
+
+        return next;
+    }
+
+    //迭代
+    private ListNode swap2(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode tmp = dummy;
+        while (tmp.next != null && tmp.next.next != null) {
+            ListNode node1 = tmp.next;
+            ListNode node2 = tmp.next.next;
+            tmp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            tmp = node1;
+        }
+        return dummy.next;
+    }
+
 }
