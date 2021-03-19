87b7761675a213a1fc56c03b92fa0fb309fbdd96
dengdaibuxizhizhou <40156530+dengdaibuxizhizhou@users.noreply.github.com>
2021-03-19 09:40:38 +0800

    Unique Paths II

--- leetcode/Algorithms/java/leetcode/src/UniquePathsII/Solution.java
+++ leetcode/Algorithms/java/leetcode/src/UniquePathsII/Solution.java
@@ -26,4 +26,26 @@ public class Solution {
         }
         return dp[m - 1][n - 1];
     }
+
+    //滚动数组优化
+    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[] dp = new int[n];
+        dp[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                    continue;
+                }
+                if (j >= 1) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
 }
