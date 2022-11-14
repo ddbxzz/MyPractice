478a4391231317d3122946bfeb585bdc9365af96
simaduan <simd@pcl.ac.cn>
2022-11-14 09:40:52 +0000

    add tuosum

--- leetcode/Algorithms/cpp/twosum/twosum.cpp
+++ leetcode/Algorithms/cpp/twosum/twosum.cpp
@@ -5,12 +5,11 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int l = 0; 
-        int r = nums.size() - 1;
+        int r = nums.size();
         for (int i =0; i < r - 1; i++) {
             for (int j = i +1; j < r; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return {nums[i], nums[j]};
+                    return {i, j};
                 }
             }
         }
