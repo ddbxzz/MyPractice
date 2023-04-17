33d77cf976b09a4da119a1463db80679303848e4
smd <sima720@163.com>
2023-04-17 09:46:38 +0800

    add other solution for subtring

--- leetcode/Algorithms/cpp/LongestSubstringWithoutRepeatingCharacters/longestSubstring.cpp
+++ leetcode/Algorithms/cpp/LongestSubstringWithoutRepeatingCharacters/longestSubstring.cpp
@@ -1,11 +1,12 @@
 #include <string>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring1(string s) {
         int ans = 0;
         int start = 0;
         unordered_map<char, int> mp;
@@ -23,5 +24,30 @@ public:
         }
 
         return ans;
+    }
+
+
+    int lengthOfLongestSubstring2(string s) {
+        vector<int> m(256, -1);
+        int res = 0, left = -1;
+        for (int i = 0; i < s.size(); ++i) {
+            left = max(left, m[s[i]]);
+            m[s[i]] = i;
+            res = max(res, i - left);
+        }
+        return res;
+    }
+
+    int lengthOfLongestSubstring3(string s) {
+        int m[256] = {0}, res = 0, left = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (m[s[i]] == 0 || m[s[i]] < left) {
+                res = max(res, i - left + 1);
+            } else {
+                left = m[s[i]];
+            }
+            m[s[i]] = i + 1;
+        }
+        return res;
     }
 };
